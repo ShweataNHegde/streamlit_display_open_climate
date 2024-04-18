a406a3138fc51b0741a9b87ab3ed7b97a87e642d
Shweata N Hegde <shweata.hegde@gmail.com>
2024-04-18 13:29:05 +0530

    multiselect with ami output

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,33 @@
 import streamlit as st
 import pandas as pd
+import numpy
 
 def get_data():
-    return pd.read_csv('country_climate.csv')
+    return pd.read_csv('country_ami.csv')
 
 def get_filter_list(df, column_name):
     countries = df[column_name].unique()
     # https://stackoverflow.com/questions/50755500/generate-a-list-from-another-list-with-comma-separated-values
-    all_countries = [single_country for country_list in countries for single_country in country_list.split(", ")]
+    all_countries = [single_country for country_list in countries for single_country in country_list.split(";")]
     all_countries_unique = list(set(all_countries))
     return all_countries_unique
 
 '## By country'
 df = get_data()
-all_countries_unique = get_filter_list(df, '0')
-all_climate_terms = get_filter_list(df,'1')
-country = st.selectbox('Country', all_countries_unique)
-climate_terms = st.selectbox('Climate Terms', all_climate_terms)
-df
-#df[df['0'].str.contains(country) & df['1'].str.contains(climate_terms)]
+
+all_countries_unique = get_filter_list(df, 'countries')
+selected_option_2 = st.multiselect("Select one or more options:", all_countries_unique)
+all_options = st.checkbox("Select all options")
+
+if all_options:
+    selected_option_2 = all_countries_unique
+#all_climate_terms = get_filter_list(df,'1')
+#country = st.selectbox('Country', selected_options)
+#climate_terms = st.selectbox('Climate Terms', all_climate_terms)
+
+df[df['countries'].str.contains('|'.join(selected_option_2))]
+#df[df['countries'].isin(selected_option_2)]
+#df[df['countries'].str.contains(country) & df['1'].str.contains(climate_terms)]
 
 #year = st.slider('Year', min_year, max_year)
 #df[df['Year'] == year]
