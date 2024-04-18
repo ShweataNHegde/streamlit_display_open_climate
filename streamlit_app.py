a802938d3ba5b8442d3a0fd98fde9ed4deae08f6
Shweata N Hegde <shweata.hegde@gmail.com>
2024-04-18 16:30:52 +0530

    with report selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,41 @@
 import streamlit as st
 import pandas as pd
-import numpy
 
-def get_data():
-    return pd.read_csv('country_ami.csv')
+CSV_FILE = 'country_ami.csv'
+SEPARATOR = ';'
 
-def get_filter_list(df, column_name):
-    countries = df[column_name].unique()
+def get_data(csv_file):
+    return pd.read_csv(csv_file)
+
+def get_report_details(df, column_name):
+    df['id'] = df[column_name].str.split("/").str.get(-2)
+    return df
+
+def get_filter_list(df, column_name, separator):
+    entities_list = df[column_name].unique()
     # https://stackoverflow.com/questions/50755500/generate-a-list-from-another-list-with-comma-separated-values
-    all_countries = [single_country for country_list in countries for single_country in country_list.split(";")]
-    all_countries_unique = list(set(all_countries))
-    return all_countries_unique
+    all_entitites = [single_entity for country_list in entities_list for single_entity in country_list.split(separator)]
+    unique_entities = list(set(all_entitites))
+    return unique_entities
 
+
+df = get_data(CSV_FILE)
+df_with_id = get_report_details(df, 'para_id')
+all_countries_unique = get_filter_list(df_with_id, 'countries', SEPARATOR)
 '## By country'
-df = get_data()
+select_country = st.multiselect("Select Countries:", all_countries_unique)
+all_options_country = st.checkbox("Select all options")
+if all_options_country:
+    select_country = all_countries_unique
+'## By IPCC Report'
+all_reports = get_filter_list(df_with_id, 'id', SEPARATOR)
+select_report = st.multiselect("Select Reports:", all_reports)
 
-all_countries_unique = get_filter_list(df, 'countries')
-selected_option_2 = st.multiselect("Select one or more options:", all_countries_unique)
-all_options = st.checkbox("Select all options")
-
-if all_options:
-    selected_option_2 = all_countries_unique
 #all_climate_terms = get_filter_list(df,'1')
 #country = st.selectbox('Country', selected_options)
 #climate_terms = st.selectbox('Climate Terms', all_climate_terms)
 
-df[df['countries'].str.contains('|'.join(selected_option_2))]
-#df[df['countries'].isin(selected_option_2)]
+# https://stackoverflow.com/questions/11350770/filter-pandas-dataframe-by-substring-criteria
+'## Filtered List of Sentences'
+df_with_id[df_with_id['countries'].str.contains('|'.join(select_country)) & df_with_id['id'].str.contains('|'.join(select_report))]
 #df[df['countries'].str.contains(country) & df['1'].str.contains(climate_terms)]
-
-#year = st.slider('Year', min_year, max_year)
-#df[df['Year'] == year]
